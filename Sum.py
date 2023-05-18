be0337952bd713c8d12d00a67094e66d808537df
Natalia Dolgacheva <98455791+Natascia75@users.noreply.github.com>
2023-05-18 13:11:48 +0200

    Add files via upload

---
+++ Sum.py
@@ -0,0 +1,25 @@
+# This is the main function of Python
+def main():
+    numbers = []
+
+    # executes "x" iterations
+    for x in range(7):
+        print("Iteration: ", x)
+        numberx = int(input("Enter your number: "))
+        numbers.append(numberx)
+
+    # print the list of numbers
+    print(numbers)
+
+    # compute the total sum of numbers
+    sum = 0
+    for y in numbers:
+        sum = sum + y
+
+    # print the final result
+    print("The total sum is: ", sum)
+    print("Thanks for watching")
+
+# This is the entry point of our code
+if __name__ == "__main__":
+    main()
